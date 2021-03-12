dc4e67f877b01b2e7340f3a9d58b3916286e37f2
Dzmitry Ramantsevich <d.ramantsevich@gmail.com>
2021-03-12 17:00:52 +0300

    feat: add validation for phone number

--- backend/controllers/registerController.php
+++ backend/controllers/registerController.php
@@ -7,7 +7,6 @@ include "../utils/logger.php";
     if(!empty($data)){
         session_start();
         $regPassword = "/^[%?^#$]?(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{6,}/";//* любое число раз подряд или отсутствовать
-
         $errorsArray = array();
 
         if($data["password"] == $data["passwordConfirm"] && $data["password"] != null){
@@ -25,16 +24,25 @@ include "../utils/logger.php";
 
         if(empty($errorsArray)){
             $email = $data["Email"];
-
             $query = "SELECT Email FROM users WHERE Email ='$email'";
-
-            $result = mysqli_query($dbLink, $query) or die ("Ошибка запроса".mysqli_error($dbLink));
+            $result = mysqli_query($dbLink, $query) or die ("Select error".mysqli_error($dbLink));
 
             if($result){
                 $row = mysqli_fetch_row($result);
-
                 if(!empty($row[0])){
                     array_push($errorsArray, "This email is registered");
+                    LogsRegFailed();
+                }
+            }
+
+            $phoneNumber = $data["phoneNumber"];
+            $query = "SELECT PhoneNumber FROM users WHERE PhoneNumber = '$phoneNumber'";
+            $result = mysqli_query($dbLink, $query) or die ("Select error".mysqli_error($dbLink));
+
+            if($result){
+                $row = mysqli_fetch_row($result);
+                if(!empty($row[0])){
+                    array_push($errorsArray, "This phone number is registered");
                     LogsRegFailed();
                 }
             }
@@ -43,13 +51,17 @@ include "../utils/logger.php";
                 $password = $data["password"];
                 $confirmPassword = $data["passwordConfirm"];
                 $name = $data["Name"];
-                $phoneNumber = $data["phoneNumber"];
                 $passwordHash = md5($password).$salt;
 
                 $query = "INSERT INTO users(Name, Email, PhoneNumber, Password) VALUES ('$name', '$email', '$phoneNumber', '$passwordHash')";
 
-                $result = mysqli_query($dbLink, $query) or die ("Ошибка запроса".mysqli_error($dbLink));
+                $result = mysqli_query($dbLink, $query) or die ("Select error".mysqli_error($dbLink));
+
                 LogsRegAccepted();
+
+//                if($result){
+//                    header("Location: 'file where need location");
+//                }
             }
         }
 
